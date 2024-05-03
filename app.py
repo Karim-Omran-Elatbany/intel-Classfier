4036506bb4e208fb33d5099420210c8458cb2a5e
Karim-Omran-Elatbany <95541499+Karim-Omran-Elatbany@users.noreply.github.com>
2024-05-03 14:22:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ app = Flask(__name__)
 
 def preprossing(image):
     image=Image.open(image)
-    image = image.resize((224, 224))
+    image = image.resize((150, 150))
     image_arr = np.array(image.convert('RGB'))
-    image_arr.shape = (1, 224, 224, 3)
+    image_arr.shape = (1, 150, 150, 3)
     return image_arr
+
 classes = ['Akhenaten', 'Bent pyramid for senefru', 'Colossal Statue of Ramesses II', 'Colossoi of Memnon', 'Goddess Isis with her child', 'Hatshepsut', 'Khafre Pyramid', 'King Thutmose III', 'Mask of Tutankhamun', 'Nefertiti', 'Pyramid_of_Djoser', 'Ramessum', 'Statue of King Zoser', 'Statue of Tutankhamun with Ankhesenamun', 'Temple_of_Isis_in_Philae', 'Temple_of_Kom_Ombo', 'The Great Temple of Ramesses II', 'amenhotep iii and tiye', 'bust of ramesses ii', 'head Statue of Amenhotep iii', 'menkaure pyramid', 'sphinx']
 model=load_model("mobilenet_model.h5")
 
@@ -22,6 +23,7 @@ def index():
 
 @app.route('/predictApi', methods=["POST"])
 def api():
+    # Get the image from post request
     try:
         if 'fileup' not in request.files:
             return "Please try again. The Image doesn't exist"
@@ -31,9 +33,9 @@ def api():
         result = model.predict(image_arr)
         print("Model predicted")
         ind = np.argmax(result)
-        prediction_class = classes[ind]
-        print(prediction_class)
-        return jsonify({'prediction': prediction_class})
+        prediction = classes[ind]
+        print(prediction)
+        return jsonify({'prediction': prediction})
     except:
         return jsonify({'Error': 'Error occur'})
 
@@ -51,14 +53,13 @@ def predict():
         result = model.predict(image_arr)
         print("predicted ...")
         ind = np.argmax(result)
-        prediction_class = classes[ind]
-        print(prediction_class)
+        prediction = classes[ind]
 
+        print(prediction)
 
-        return render_template('index.html', prediction=prediction_class,  image='static/IMG/', appName="Intel Image Classification")
+        return render_template('index.html', prediction=prediction, image='static/IMG/', appName="Intel Image Classification")
     else:
         return render_template('index.html',appName="Intel Image Classification")
-
 
 
 if __name__ == '__main__':
